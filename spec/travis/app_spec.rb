cc17fbf5484be1d51b3bf5a2576eb735814c82f6
Hiro Asari <asari.ruby@gmail.com>
2015-07-11 10:05:27 -0400

    Add spec for GET /cache

--- spec/travis/app_spec.rb
+++ spec/travis/app_spec.rb
@@ -15,4 +15,16 @@ describe Travis::Caching::App, :include_sinatra_helpers do
       expect(response.status).to be == 204
     end
   end
+
+  describe 'GET /cache' do
+    let(:token) {
+      # payload with no time stamps
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJUcmF2aXMgQ0ksIEdtYkgiLCJjb250ZXh0Ijp7InNsdWciOiJ0cmF2aXMtY2kvdHJhdmlzLWNpIiwiYnJhbmNoIjoibWFzdGVyIiwibGFuZ3VhZ2UiOiJydWJ5In19.FcN-ncXaziPZZHJectxTUkX2pjrIcQi9Tmcxugtrufo'
+    }
+
+    it 'decodes payload correctly' do
+      response = get "/cache?token=#{token}"
+      expect(response.status).to be == 200
+    end
+  end
 end
